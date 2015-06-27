c2544940cd580300fb5d51962c7c9b44d3bd5428
Nick Hamann <nick@wabbo.org>
2015-06-27 00:47:12 -0500

    Pull out some basic fields from torrent file

--- src/metainfo.rs
+++ src/metainfo.rs
@@ -1,4 +1,5 @@
 use bencode::{self, FromBencode, Bencode};
+use std::collections::BTreeMap;
 use std::path::PathBuf;
 use std::fs::File;
 use std::fmt;
@@ -13,13 +14,19 @@ pub struct MetaInfo {
     // announce URL of tracker
     announce: String,
 
+    // in Unix epoch format
+    creation_date: Option<String>,
+
     // name and version of program that created the torrent file
-    created_by: String, 
+    created_by: Option<String>,
+
+    // encoding used for `pieces` portion of info dictionary
+    encoding: Option<String>,
 }
 
 impl fmt::Debug for MetaInfo {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        write!(f, "MetaInfo {{ announce: {}, created_by: {} }}",
+        write!(f, "MetaInfo {{ announce: {:?}, created_by: {:?} }}",
                self.announce, self.created_by)
     }
 }
@@ -56,14 +63,52 @@ struct MultipleFileIndividualFileInfo {
 }
 
 
+pub type DecodeError = String;
+
+#[derive(Debug)]
+enum MetaInfoFieldType {
+    ByteString(String),
+    Number(i64),
+}
+
+// extract a Bencoded ByteString field from a BTreeMap (by key)
+fn get_field(map: &BTreeMap<bencode::util::ByteString, Bencode>,
+             key: &bencode::util::ByteString)
+        -> Result<MetaInfoFieldType, DecodeError> {
+    match map.get(key) {
+        Some(&Bencode::ByteString(ref s)) => {
+            match String::from_utf8(s.clone()) {
+                Ok(s) => Ok(MetaInfoFieldType::ByteString(s)),
+                Err(e) => Err(format!("Error: {}", e)),
+            }
+        },
+        Some(&Bencode::Number(x)) => Ok(MetaInfoFieldType::Number(x)),
+        Some(_) => Err(format!("{:?}'s value in the dictionary is not a string",
+                               String::from_utf8(key.clone().unwrap() ))),
+        None => Err(format!("{:?} not found in the dictionary", key)),
+    }
+}
+
+
 impl FromBencode for MetaInfo {
-    type Err = &'static str;
+    type Err = DecodeError;
     fn from_bencode(b: &Bencode) -> Result<MetaInfo, Self::Err> {
         match *b {
-            Bencode::Dict(ref d) => {
+            Bencode::Dict(ref m) => {
+                let announce_key = &bencode::util::ByteString::from_str("announce");
+                let announce = try!(get_field(m, announce_key));
+                let created_by_key = &bencode::util::ByteString::from_str("created by");
+                let created_by = try!(get_field(m, created_by_key));
+                let creation_date_key = &bencode::util::ByteString::from_str("creation date");
+                let creation_date = try!(get_field(m, creation_date_key));
+                let encoding_key = &bencode::util::ByteString::from_str("encoding");
+                let encoding = try!(get_field(m, encoding_key));
+                println!("announce = {:?}, creation_date = {:?},
+                          created by = {:?}, encoding = {:?}", announce, creation_date,
+                                                               created_by, encoding);
                 unimplemented!()
             },
-            _ => Err("Bencoded string is not a dictionary."),
+            _ => Err(format!("Bencoded string is not a dictionary.")),
         }
     }
 }
@@ -72,7 +117,7 @@ impl FromBencode for MetaInfo {
 pub enum ParseError {
     IoError(io::Error),
     BencodeDecodingError(bencode::streaming::Error),
-    Other(&'static str),
+    Other(String),
 }
 
 impl From<io::Error> for ParseError {
